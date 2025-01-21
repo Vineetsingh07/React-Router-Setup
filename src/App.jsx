c3fc85d75bdcf33e579f575ecd19114e05a697d1
Vineetsingh07 <vineetsingh2603@gmail.com>
2025-01-21 19:14:59 +0530

    Add Github Route.

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Home from "./components/Home/Home";
 import About from "./components/About/About";
 import Contact from "./components/Contact/Contact";
 import User from "./components/User/User";
+import Github from "./components/Github/Github";
 
 function App() {
   // const router = createBrowserRouter([
@@ -35,7 +36,9 @@ function App() {
         <Route path="about" element={<About />} />
         <Route path="contact" element={<Contact />} />
         <Route path="user/:userId" element={<User />} />
-        {/* <Route loader={githubInfoLoader} path="github" element={<Github />} /> */}
+        <Route 
+        // loader={githubInfoLoader} 
+        path="github" element={<Github />} />
       </Route>
     )
   );
